a12c8b03d490dffab8041c30f1024f027ce03e13
Lumikin <lucas.m.ribeiro11@aluno.senai.br>
2026-08-13 14:59:00 -0300

    Revert "Arrumando erros"
    This reverts commit 70ed2f2f43b745f702e268efd0fc33f50e354d8b.

--- mobile/screens/login-screen.js
+++ mobile/screens/login-screen.js
@@ -10,7 +10,7 @@ import {
   SafeAreaView,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import LoginForm from '../components/login-forms';
+import LoginForm from '../components/login-form';
 import { BrandColors } from '../constants/brand-colors';
 
 export default function LoginScreen() {
@@ -26,7 +26,7 @@ export default function LoginScreen() {
         >
           <View style={styles.header}>
             <Image
-              source={require('../assets/icon.png')}
+              source={require('../assets/logo.png')}
               style={styles.logo}
               resizeMode="contain"
             />
